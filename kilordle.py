45fe50fe9bdd3ee6aac6aa6b63e682cf096ff8b4
Imogen <me@imogen.tech>
2022-04-08 19:57:58 +0200

    blep

--- kilordle.py
+++ kilordle.py
@@ -4,6 +4,6 @@ from string import ascii_lowercase
 
 letter_counts = {l: {i: sum(1 for w in wordles if w[i] == l) for i in range(5)} for l in ascii_lowercase}
 
-missing = {l: {i: 1 for i, j in c.items() if j == 0} for l, c in letter_counts.items()}
-missing = {l: d for l, d in missing.items() if d != {}}
-print(missing)
+missing = {l: {i for i, j in c.items() if j == 0} for l, c in letter_counts.items()}
+#print({l: d for l, d in missing.items() if d != {}})
+
