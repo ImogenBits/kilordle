33eaf50baed6ed55bc6f19b36ca88b018afe0c9c
Imogen <me@imogen.tech>
2022-04-08 23:18:21 +0200

    nlp

--- kilordle.py
+++ kilordle.py
@@ -1,9 +1,23 @@
 from string import ascii_lowercase
 from words import words, wordles
 from string import ascii_lowercase
+from pulp import LpVariable, const, LpProblem, LpMinimize, lpSum
 
 letter_counts = {l: {i: sum(1 for w in wordles if w[i] == l) for i in range(5)} for l in ascii_lowercase}
 
-missing = {l: {i for i, j in c.items() if j == 0} for l, c in letter_counts.items()}
+needed = {l: {i for i, j in c.items() if j != 0} for l, c in letter_counts.items()}
 #print({l: d for l, d in missing.items() if d != {}})
 
+problem = LpProblem("kilordle", LpMinimize)
+variables = {w: LpVariable(w, 0, 1, const.LpInteger) for w in words}
+problem += lpSum([variables[w] for w in wordles])
+
+for letter, pos in needed.items():
+    for i in pos:
+        problem += lpSum([variables[w] for w in wordles if w[i] == letter]) >= 1
+
+status = problem.solve()
+
+print(status)
+
+print(123)
