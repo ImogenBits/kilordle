7493e8472cdb2870868ec83f85d9329b92fc1888
Imogen <me@imogen.tech>
2022-04-08 19:41:43 +0200

    blep

---
+++ kilordle.py
@@ -0,0 +1,9 @@
+from string import ascii_lowercase
+from words import words, wordles
+from string import ascii_lowercase
+
+letter_counts = {l: {i: sum(1 for w in wordles if w[i] == l) for i in range(5)} for l in ascii_lowercase}
+
+missing = {l: {i: 1 for i, j in c.items() if j == 0} for l, c in letter_counts.items()}
+missing = {l: d for l, d in missing.items() if d != {}}
+print(missing)
