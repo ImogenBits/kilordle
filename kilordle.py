db8926d4247b3588820622cfd1b79fdfc76e17b8
Imogen <me@imogen.tech>
2022-04-08 23:45:35 +0200

    final

--- kilordle.py
+++ kilordle.py
@@ -1,23 +1,25 @@
 from string import ascii_lowercase
 from words import words, wordles
 from string import ascii_lowercase
-from pulp import LpVariable, const, LpProblem, LpMinimize, lpSum
+from pulp import LpVariable, const, LpProblem, LpMinimize, lpSum, LpStatus
 
 letter_counts = {l: {i: sum(1 for w in wordles if w[i] == l) for i in range(5)} for l in ascii_lowercase}
-
 needed = {l: {i for i, j in c.items() if j != 0} for l, c in letter_counts.items()}
-#print({l: d for l, d in missing.items() if d != {}})
 
 problem = LpProblem("kilordle", LpMinimize)
-variables = {w: LpVariable(w, 0, 1, const.LpInteger) for w in words}
-problem += lpSum([variables[w] for w in wordles])
+all_words = words | wordles
+variables = {w: LpVariable(w, 0, 1, const.LpInteger) for w in all_words}
+problem += lpSum([variables[w] for w in all_words])
 
 for letter, pos in needed.items():
     for i in pos:
-        problem += lpSum([variables[w] for w in wordles if w[i] == letter]) >= 1
+        problem += lpSum([variables[w] for w in all_words if w[i] == letter]) >= 1
 
-status = problem.solve()
+problem.solve()
+print(LpStatus[problem.status])
 
-print(status)
+best_words = {v.name for v in problem.variables() if v.varValue == 1}
+print(f"Best solution has {len(best_words)} words: {', '.join(best_words)}")
 
-print(123)
+sorted_words = [sorted(best_words, key=lambda w: w[i]) for i in range(5)]
+print("\n".join(" ".join(sorted_words[i][j] for i in range(5)) for j in range(len(sorted_words[0]))))
